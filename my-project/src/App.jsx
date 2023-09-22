a5591a2ed9331adeb3933eb8385c9e668ef4e908
anatolievich21 <pastushenkovladislav21@gmail.com>
2023-09-22 13:20:56 +0300

    Add CSS styles to index.scss
    Global styles for the `body` element to set fonts, margins, and background color for a consistent look.
    2. Styling for the `.App` component to center its content vertically and horizontally, adjust text alignment, and maintain a responsive design.
    3. Custom styles for the counter (`h1`) with different colors for "Plus" and "Minus" modes.
    4. Styling for buttons (`button`) with transparent backgrounds, borders, hover effects, and active states to enhance user interactions.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,25 +2,27 @@ import { useState } from "react";
 import './index.scss'
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const onClickMinus = () => {
-    setCount(count - 1);
-  };
+    const onClickMinus = () => {
+        setCount(count - 1);
+    };
 
-  const onClickPlus = () => {
-    setCount(count + 1);
-  };
+    const onClickPlus = () => {
+        setCount(count + 1);
+    };
 
 
-  return (
-      <div>
-        <h2>Counter</h2>
-        <h1>{count}</h1>
-        <button className="minus" onClick={onClickMinus}>- Minus</button>
-        <button className="plus" onClick={onClickPlus}>Plus +</button>
-      </div>
-  )
+    return (
+        <div className="App">
+            <div>
+                <h2>Counter</h2>
+                <h1>{count}</h1>
+                <button className="minus" onClick={onClickMinus}>- Minus</button>
+                <button className="plus" onClick={onClickPlus}>Plus +</button>
+            </div>
+        </div>
+    )
 }
 
 export default App
