b3c25a8b8efaaa6ea5c005dcc5a4b85f3bdc1dc8
anatolievich21 <pastushenkovladislav21@gmail.com>
2023-09-22 11:59:56 +0300

    Add counter functionality to App component (Fix site icon path)
    I've implemented the counter functionality in the App component. The changes include:
    - Added state management using useState to track the count.
    - Created two buttons, one for incrementing and one for decrementing the count.
    - Defined event handlers to update the count when the buttons are clicked.
    
    Now, the App component can accurately display and manipulate the count, making it a functional counter.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import './index.scss'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+
 
   return (
-    <div>
-        
-    </div>
+      <div>
+        <h2>Counter</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>- Minus</button>
+        <button className="plus" onClick={onClickPlus}>Plus +</button>
+      </div>
   )
 }
 
